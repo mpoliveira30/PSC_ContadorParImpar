341bbf761b6127b45aaddcc7b453209eeca5040a
Matheus de Paula Oliveira <105546758+mpoliveira30@users.noreply.github.com>
2022-09-09 20:58:43 -0300

    Add files via upload

---
+++ ParImpar/src/main/java/com/mycompany/parimpar/Par.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.parimpar;
+
+/**
+ *
+ * @author Sany
+ */
+public class Par {
+    public static void main(String[] args) {
+        
+       String nome = "";
+        
+        for (int contador = 1 ; contador <=50 ; contador++){
+            if (contador % 2 == 0) {
+                System.out.println("O Valor do contador é: " +contador);
+            }
+            
+        }
+        
+    }
+}
